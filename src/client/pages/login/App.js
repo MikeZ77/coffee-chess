dc6919ec3dab934739ca896b263a4f44c7475bb0
Michael Zaghi <Michael.Zaghi19@gmail.com>
2022-09-16 21:31:10 -0600

    Testing different UI fraeworks and setting up development environment.

---
+++ src/client/pages/login/App.js
@@ -0,0 +1,22 @@
+import '../../common/bulma.styles.scss';
+import createElement from 'virtual-dom/create-element';
+import hh from 'hyperscript-helpers';
+import { h } from 'virtual-dom';
+
+const { button, div } = hh(h);
+
+const node = document.getElementById('app');
+// let btn = createElement(button({className: 'button'}, 'Hello'))
+
+function testLayout() {
+  return div({ className: 'columns is-centered' }, [
+    div({ className: 'column is-one-third' }, [
+      button({ className: 'button' }, 'Hello1')
+    ]),
+    div({ className: 'column' }, [button({ className: 'button' }, 'Hello2')]),
+    div({ className: 'column' }, [button({ className: 'button' }, 'Hello3')])
+  ]);
+}
+
+const view = createElement(testLayout());
+node.appendChild(view);
